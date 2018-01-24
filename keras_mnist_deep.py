51b4eb88f39278d376a5bc7ba9b9b8a2161fdf4a
Keelin Murphy <keelinm@gmail.com>
2018-01-24 22:08:39 +0000

    added extras

--- keras_mnist_deep.py
+++ keras_mnist_deep.py
@@ -35,6 +35,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 from scipy import misc
 
 import tensorflow as tf
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -82,6 +85,81 @@ def main(_):
     test_images = np.reshape(mnist.test.images, [-1, 28, 28, 1])
     accuracy_test = my_model.evaluate(test_images, mnist.test.labels, batch_size=50)
     print('\n\ntest accuracy is ', accuracy_test)
+
+    # Test on individual test examples, writing examples of 
+    # successful and failed classifications to disk
+    if FLAGS.write_samples:
+        file_prefix = ''
+        if 'fashion' in FLAGS.data_dir:
+            file_prefix = 'fashion_deep_'
+            if not os.path.exists(os.path.join(os.getcwd(), 'output_images')):
+                os.makedirs(os.path.join(os.getcwd(), 'output_images'))
+            num_each_to_store = 5
+            stored_correct = 0
+            stored_incorrect = 0
+            idx = 0
+            while (stored_correct < num_each_to_store or stored_incorrect < num_each_to_store) and idx < len(mnist.test.images):
+                pred = np.argmax(my_model.predict(np.reshape(mnist.test.images[idx], [-1, 28, 28, 1])))
+                real_label = np.argmax(mnist.test.labels[idx])
+                correct = pred == real_label
+
+                if file_prefix is 'fashion_deep_':
+                    real_label = fashion_label_to_name(real_label)
+                    pred = '[' + fashion_label_to_name(pred) + ']'
+                else:
+                    real_label = real_label.astype(str)
+                    pred = pred.astype(str)
+
+                img = np.reshape(mnist.test.images[idx], [28, 28])
+                plt.imshow(img, cmap='gray')
+
+                if correct and stored_correct < num_each_to_store:
+                    stored_correct += 1
+                    plt.savefig("output_images/{}success_{}.png".format(file_prefix, real_label))
+                elif not correct and stored_incorrect < num_each_to_store:
+                    stored_incorrect += 1
+                    plt.savefig("output_images/{}fail_{}_{}.png".format(file_prefix, real_label, pred))
+                idx += 1
+
+    # Test on extra test images made from photos of handwritten digits
+    # or from digitally created 'hand' written digits
+    if FLAGS.extra_test_imgs:
+        if not os.path.exists(os.path.join(os.getcwd(), 'output_images')):
+            os.makedirs(os.path.join(os.getcwd(), 'output_images'))
+        file_prefix = 'extra_'
+        for idx in range(1, 7):
+            img_file = 'extra_test_digits/{}.jpg'.format(idx)
+            img = misc.imread(img_file)
+            pred = np.argmax(my_model.predict(np.reshape(img, [1, 28, 28, 1])))
+
+            plt.imshow(img, cmap='gray')
+            plt.savefig("output_images/{}{}predicted_{}.png".format(file_prefix, idx, pred))
+
+
+# function to convert fashion MNIST label (number) to clothing type string
+def fashion_label_to_name(label):
+    if label == 0:
+        return 'tshirt_top'
+    elif label == 1:
+        return 'trousers'
+    elif label == 2:
+        return 'pullover'
+    elif label == 3:
+        return 'dress'
+    elif label == 4:
+        return 'coat'
+    elif label == 5:
+        return 'sandal'
+    elif label == 6:
+        return 'shirt'
+    elif label == 7:
+        return 'sneaker'
+    elif label == 8:
+        return 'bag'
+    elif label == 9:
+        return 'ankle_boot'
+    else:
+        return 'category_unknown'
 
 
 if __name__ == '__main__':
