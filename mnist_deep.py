39ebdbbd3d88a22f0a7cc34ede77d178a0aad3ce
Keelin Murphy <keelinm@gmail.com>
2018-01-24 13:15:27 +0000

    original tf convnet

--- mnist_deep.py
+++ mnist_deep.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+# A copy of the version from Meetup1
 
 """A deep MNIST classifier using convolutional layers.
 
